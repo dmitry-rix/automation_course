874353c6119db07ee91909fb37f48844e1d36624
dmitry-rix <dmitrij.ryzhikov.88@mail.ru>
2023-03-31 17:47:41 +0400

    check git

--- task_1/tests/test_item_price_response.py
+++ task_1/tests/test_item_price_response.py
@@ -8,3 +8,5 @@ def test_item_price_response():
     response = requests.get(f"{BASE_URL}/products/i511400f/prices-history?period=12m")
 
     assert response.status_code == 200, GlobalErrorMessages.INVALID_STATUS_CODE.value
+
+    # comment for visibility
